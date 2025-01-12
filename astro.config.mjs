ef34d300e05e09df2cd0784c35e99df8d994def2
Andriamihaja <manu.andriamihaja-razafimanjato@etu.umontpellier.fr>
2025-01-12 01:04:54 +0100

    Base du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import {defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://harutsukiGo.github.io',
-    base: '/portfolio-2025/',
+    base: '/portfolio-2025-2/',
 });
